2a75687654eb570c80efdd2f82f664e15a8172b7
Jean de Dieu Uwimana <johnchafi@gmail.com>
2024-09-11 14:59:58 -0400

    first commit

---
+++ middleware.ts
@@ -0,0 +1,32 @@
+import { clerkMiddleware , createRouteMatcher} from "@clerk/nextjs/server";
+ 
+// export default clerkMiddleware({
+//   publicRoutes: [
+//     '/',
+//     '/events/:id',
+//     '/api/webhook/clerk',
+//     '/api/webhook/stripe',
+//     '/api/uploadthing'
+//   ],
+//   ignoredRoutes: [
+//     '/api/webhook/clerk',
+//     '/api/webhook/stripe',
+//     '/api/uploadthing'
+//   ]
+// });
+
+const isPublicRoute = createRouteMatcher([  '/',
+  '/events/:id',
+  '/api/webhook/clerk',
+  '/api/webhook/stripe',
+  '/api/uploadthing','/sign-in(.*)', '/sign-up(.*)'])
+
+export default clerkMiddleware((auth, request) => {
+  if (!isPublicRoute(request)) {
+    auth().protect()
+  }
+})
+ 
+export const config = {
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+};
