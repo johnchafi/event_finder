ce9d2dc5721077d7b0c7dd757129a090887e7b83
johnchafi <johnchafi@gmail.com>
2024-09-24 13:31:27 -0400

    debug connection

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -8,10 +8,12 @@ let cached = (global as any).mongoose || {conn: null, promise: null}
  const connectToDatabase = async () => {
     if(cached.conn) return cached.connect;
     if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
+    console.log('Before connect => ' + MONGODB_URI);
     cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
         dbName: 'evently',
         bufferCommands: false,
     })
+    console.log('After connect => ' + MONGODB_URI);
     cached.conn = await cached.promise
     console.log('connectedToDatabase => ' + cached.conn);
     return cached.conn;
