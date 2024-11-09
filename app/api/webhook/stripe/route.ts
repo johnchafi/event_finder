d4139eb2e8699f0533089405c369d00259b377b6
johnchafi <johnchafi@gmail.com>
2024-11-08 22:13:05 -0500

    test

--- app/api/webhook/stripe/route.ts
+++ app/api/webhook/stripe/route.ts
@@ -66,8 +66,8 @@ export async function POST(request: Request) {
       createdAt: new Date(),
     }
 
-    const newOrder = await createOrder(order);
-    return NextResponse.json({ message: 'OK', order: newOrder })
+    //const newOrder = await createOrder(order);
+    //return NextResponse.json({ message: 'OK', order: newOrder })
   }
 
   return new Response('', { status: 200 })
