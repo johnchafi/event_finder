a36b86aa5143eb4b6719082ff26abda2804b04e2
johnchafi <johnchafi@gmail.com>
2025-01-20 12:25:46 -0500

    try fix build error

--- app/(root)/explore/page.tsx
+++ app/(root)/explore/page.tsx
@@ -35,23 +35,6 @@ const pastEvents = events?.data.filter((event:any) => Date.parse(event.startDate
   return (
     <>
     <section className="bg-dotted-pattern pattern bg-contain py-5 md:py-10">
-      {/* <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
-        <div className="flex flex-col justify-center gap-8">
-          <h1 className="h1-bold"><span className="h1-bold text-primary-800"> Host, connect , </span>  celebrate: Your <span className="h1-bold text-primary-800"> events , </span> our platform!</h1>
-          <p className="p-regular-12 md:p-regular-16">Eventhub simplifies event planning by allowing you to manage guests, sell tickets, coordinate details, and connect seamlessly. It’s the ultimate solution for hassle-free event enjoyment!</p>
-          <Link className="button w-full" href="#events">
-            <MagicButton
-              title=" Explorer Now"
-              icon={<NavigationIcon className='w-4'/>}
-              position="right"
-            />
-          </Link>
-        </div>
-        
-        <InfiniteCarousel items={heroImages} />
-
-      </div> */}
-   
       <section id="events" className="wrapper w-full my-8 flex flex-col gap-8 text-center">
         <div className="flex w-full flex-col gap-5 md:flex-row">
           <Search />
@@ -89,13 +72,6 @@ const pastEvents = events?.data.filter((event:any) => Date.parse(event.startDate
           page={1}
           totalPages={2}
         />
-
-      {/* <h2 className="h2-bold">Trusted by many <span className="text-primary-800">organizers</span></h2>
-        <InfiniteMovingCards
-            items={testimonials}
-            direction="left"
-            speed="fast"
-        /> */}
       </section>
       
 
